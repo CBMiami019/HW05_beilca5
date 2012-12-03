28040354543d85d52e5b7718806cf1e3c7022364
Carter Beil <beilca@muohio.edu>
2012-12-03 14:02:47 -0500

    Finished.
    I was having trouble near the end reading in my Starbucks.csv file, but
    the errors have gone away. I did not get around to making a graphics
    visualization for this assignment. The logic behind it was enough work
    trying to understand.

--- HW05_beilca5/GraphAlgs.cpp
+++ HW05_beilca5/GraphAlgs.cpp
@@ -1,7 +1,9 @@
 #include "GraphAlgs.h"
 #include <vector>
+#include <algorithm>
 
 using namespace std;
+
 
 /*
  * Solves the Traveling Salesperson Problem: finding the shortest cycle through a graph that 
@@ -21,7 +23,7 @@ using namespace std;
 //Trying to use the notes taken in class to make this algorithm work
 
 //We need a few variables to make this work
-double shortestPathLength = 0;
+double shortestPathLength = 999999999;
 double currentPathLength = 0;
 
 //Since we will be returning a vector that is the best path, we need a variable for it
@@ -30,10 +32,56 @@ vector<NodeID> currentPath;
 
 //Citation for next_permutation comes from many classmates and
 //http://wordaligned.org/articles/next-permutation specifically the"Next_permutation In Action" section
+/*
+Next_permutation transforms the range of elements [first, last) into the lexicographically next greater
+permutation of the elements. […] If such a permutation exists, next_permutation transforms [first, last)
+into that permutation and returns true. Otherwise it transforms [first, last) into the lexicographically 
+smallest permutation and returns false.
+*/
 
 std::pair<std::vector<NodeID>, EdgeWeight> TSP(Graph* G){
-	for(int i = 1; i < G->size(); i++)
-		currentPath.push_back(i);
-
+	//Citation:help from Adam Rasfeld in person, as well as Jake Rego, thanks for the notes!
+	//Add the current graph to an array to hold the values 
+	//We will be using this later to store the values of our path, in our vector BestPath
+	//and call it numerous times on currentPath
+	int graphSize = G->size();
+	int* tempArray = new int[graphSize];
+	for(int i = 0; i < graphSize; i++)
+		tempArray[i] = i;
 	
+	do {
+		double totalDistance = 0;
+		// The first step that we need to take is to add up the total distance of all the nodes linked together
+		//and see if that distance is greater than the current BestLength or not.
+		for(int i = 0; i < graphSize; i++) {
+			// Add weight between last node and first node
+			if(i == graphSize-1)
+				//G->weight gets the distance between the two points, from the tempArray
+				totalDistance += G->weight(tempArray[i],tempArray[0]);
+			// Add weight between nodes
+			else
+				//G->weight gets the distance between the two points, from the tempArray
+				totalDistance += G->weight(tempArray[i],tempArray[i+1]);
+		}
+		//If this length is the best length so far, then we need to change the shortestPathLength to the current
+		//distance of the bestPath. 
+		if(totalDistance < shortestPathLength) {
+			shortestPathLength = totalDistance;
+			// Set as current best path
+			for(int j = 0; j < graphSize; j++)
+				BestPath[j] = tempArray[j];
+		}
+	/*
+	Next_permutation transforms the range of elements [first, last) into the lexicographically next greater
+	permutation of the elements. […] If such a permutation exists, next_permutation transforms [first, last)
+	into that permutation and returns true. Otherwise it transforms [first, last) into the lexicographically 
+	smallest permutation and returns false.
+	*/
+	} while (next_permutation(tempArray, tempArray+graphSize));
+
+	/*Return value: Return a pair, with the first element being a vector of length n listing the 
+	 *  order of the nodes in the cycle (do NOT list the start node twice), and the second element
+	 *  being the length of that path
+	 */
+	return pair<vector<NodeID>,EdgeWeight>(BestPath,shortestPathLength);
 }
